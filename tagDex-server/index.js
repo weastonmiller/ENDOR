0f0619318776e8152ae9c90f31b0ee9169f753bd
Weston Miller <westongmiller@gmail.com>
2023-02-19 17:30:57 -0500

    Initial commit

---
+++ tagDex-server/index.js
@@ -0,0 +1,37 @@
+import express from "express";
+import http from "http";
+import { ApolloServer } from "@apollo/server";
+import cors from "cors";
+
+// Using middleware implementation because unsure of auth scheme currently
+import { expressMiddleware } from "@apollo/server/express4";
+import { ApolloServerPluginDrainHttpServer } from "@apollo/server/plugin/drainHttpServer";
+
+async function init() {
+  const app = express();
+  const httpServer = http.createServer(app);
+
+  const server = new ApolloServer({
+    plugins: [ApolloServerPluginDrainHttpServer({ httpServer })],
+  });
+
+  await server.start();
+
+  // leaving middleware empty for now till auth scheme decision
+  app.options(
+    "/api/gql",
+    cors(),
+    json(),
+    expressMiddleware(server, {
+      context: async (c) => {
+        return;
+      },
+    })
+  );
+
+  await new Promise((resolve) => httpServer.listen({ port: 4000 }, resolve));
+
+  console.log(`Server running at port 4000`);
+}
+
+init();
